38936cf06dcf9ccf029941f2cfafdd119dfe5814
Revtm <kykylizaro98@gmail.com>
2019-12-22 23:33:32 +0700

    add tugas

---
+++ sql/java/buat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo javac tableGen4.java
+sudo java tableGen4
+
+sudo javac tableGen5.java
+sudo java tableGen5
+
+sudo javac tableGen6.java
+sudo java tableGen6
+
+sudo javac tableGen7.java
+sudo java tableGen7
---
+++ sql/java/sql/all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo mysql -u root mbdrevan < all.sql
+sudo mysql -u root mbdrevan2 < all2.sql
+sudo mysql -u root mbdrevan3 < all3.sql
+sudo mysql -u root mbdrevan4 < all4.sql
